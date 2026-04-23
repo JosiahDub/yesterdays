e4ebc3d32ba02338ccbf00e611cadb1a1f3d3e9b
Josiah White <josiahwhite@gmail.com>
2026-04-22 21:49:02 -0700

    Sorting file name

--- images/management/commands/importers/weber.py
+++ images/management/commands/importers/weber.py
@@ -74,7 +74,7 @@ def handle(options):
     )
     r2_uploader = R2Uploader()
 
-    for index, file_name in enumerate(os.listdir(image_dir)):
+    for index, file_name in enumerate(sorted(os.listdir(image_dir))):
 
         title, year = TITLES[index]
         year = str(year)
